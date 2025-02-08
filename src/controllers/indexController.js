53d7e6489387b5dbb01409f7475e22f7028c65c7
chrwiencke <81775007+chrwiencke@users.noreply.github.com>
2025-02-08 22:40:48 +0100

    Asddd Redis caching to organization queries in indexController

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,11 +1,23 @@
 const brreg = require('../models/brregModel.js');
 
+const { createClient } = require('redis');
+
+const client = createClient({
+    url: 'redis://10.12.2.99:6379',
+});
+client.connect();
+
 const orgQuery = async (req, res) => {
     try {
         const { orgNumber } = req.body;
 
         if (!orgNumber) {
             return res.status(422).json({ message: "No organization number provided" });
+        }
+
+        const cacheQuery = await client.get(orgNumber);
+        if (cacheQuery) {
+            return res.status(200).json(JSON.parse(cacheQuery));
         }
 
         const data = await brreg.findOne({ organisasjonsnummer: orgNumber });
@@ -32,11 +44,19 @@ const nameQuery = async (req, res) => {
         if (typeof name !== "string") {
             return res.status(400).json({ message: "Invalid name provided" });
         }
+
+        const cacheQuery = await client.get(name);
+        if (cacheQuery) {
+            return res.status(200).json(JSON.parse(cacheQuery));
+        }
+
         const exactMatchQuery = brreg.findOne({ navn: { $eq: name } }).limit(5);
 
         const similarMatchQuery = brreg.findOne(
             { $text: { $search: name } },
-            { score: { $meta: 'textScore' } }).sort({ score: { $meta: 'textScore' } }).limit(5);
+            { score: { $meta: 'textScore' } })
+            .sort({ score: { $meta: 'textScore' } })
+            .limit(5);
 
         const timeout = new Promise((_, reject) => {
             setTimeout(() => {
@@ -50,16 +70,13 @@ const nameQuery = async (req, res) => {
             timeout
         ]);
 
-        if (result) {
-            return res.status(200).json(result);
-        }
-
-
         if (!result) {
             return res.status(404).json({ message: "Organization not found" });
         }
 
-        return res.status(200).json(similarMatch);
+        await client.set(name, JSON.stringify(result), { EX: 60 });
+        return res.status(200).json(result);
+
     } catch(error) {
         console.error('Query error:', error);
         return res.status(500).json({ message: 'Internal server error' });
